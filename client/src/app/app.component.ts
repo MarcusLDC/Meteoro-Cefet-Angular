8f26b08d394636bad4a6e76d7baab51e58f7f891
Marcus Lima <marcusv2001@gmail.com>
2023-03-13 18:51:46 -0300

    Primeira versão

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'Meteoro-Cefet';
-}
+  form : FormGroup;
+  periodosGrafico = [
+    { value: "1 minuto", key: 1 },
+    { value: "10 minutos", key: 2 },
+    { value: "30 minutos", key: 3 },
+    { value: "1 hora", key: 4 },
+    { value: "24 horas", key: 5 },
+    { value: "Mensal", key: 6 }
+  ];
+
+  constructor(private builder:FormBuilder) {
+      this.form = builder.group({
+          data:[null, Validators.required]
+      });
+  }
+
+  public selectIntervaloHandler() {
+    var element = document.getElementById('mensagem') as HTMLElement;
+    var el = document.getElementById('intervalo') as HTMLInputElement;
+    if (el.value == '1') {
+      element.style.display = '';
+      element.innerHTML = ` - <b class='destaque'>Aviso:</b> Não é possível criar gráficos para esse intervalo.`;
+    }
+    if (el.value == '600') {
+      element.style.display = '';
+      element.innerHTML = ` - <b class='destaque'>Aviso:</b> Apenas gráficos de até 4 dias para 1 estação ou 2 dias para 2 estações podem ser feitos.`;
+    }
+    if (el.value != '600' && el.value != '1') {
+      element.style.display = 'none';
+    }
+  }
+} 
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,6 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDatepickerModule } from '@angular/material/datepicker'
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule, MAT_DATE_LOCALE } from '@angular/material/core';
+import { MatCheckboxModule} from '@angular/material/checkbox';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,9 +19,22 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatNativeDateModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    MatDatepickerModule,
+    MatNativeDateModule,
+    {provide: MAT_DATE_LOCALE, useValue: 'pt-BR'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
