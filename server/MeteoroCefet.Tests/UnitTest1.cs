07af01d5ff88dfd5dda3d5a6d6ddc62cc31334bc
Marcus Lima <marcusv2001@gmail.com>
2023-03-16 17:07:30 -0300

    Fix

---
+++ server/MeteoroCefet.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace MeteoroCefet.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
