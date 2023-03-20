98399ab829aeaf9f765895cc85b341f80a1d7d6f
João Xavier <fastjonh@gmail.com>
2023-03-20 12:36:32 -0300

    implemented method to receive new data

---
+++ server/MeteoroCefet.API/Endpoints/NovoDadoTempoEndpoint.cs
@@ -0,0 +1,14 @@
+﻿namespace MeteoroCefet.API.Endpoints
+{
+    public class NovoDadoTempoEndpoint : IEndpointDefinition
+    {
+        public void DefineEndpoints(WebApplication app)
+        {
+            app.MapPost("dados/new", Handler);
+        }
+        private static async Task<string> Handler(string msg, string key)
+        {
+            return msg;
+        }
+    }
+}
