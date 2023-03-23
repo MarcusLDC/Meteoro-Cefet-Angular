108b71c8dc98dfa6274bed85a9a5576f9f23939a
Marcus Lima <marcusv2001@gmail.com>
2023-03-23 18:09:17 -0300

    Implemented new Component and Endpoint to edit and show Estacao content.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ConsultaComponent } from './consulta/consulta.component';
 import { DadosComponent } from './dados/dados.component';
+import { EstacoesComponent } from './estacoes/estacoes.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path: "dados", component: DadosComponent},
-  {path: "consulta", component: ConsultaComponent}
+  {path: "consulta", component: ConsultaComponent},
+  {path: "estacoes", component: EstacoesComponent},
+  {path: "login", component: LoginComponent}
 ];
 
 @NgModule({
