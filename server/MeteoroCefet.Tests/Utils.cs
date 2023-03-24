dbb6f8a11767797e44b0bf910fc2ad80fca546dc
João Xavier <fastjonh@gmail.com>
2023-03-24 00:25:27 -0300

    criando metodo pra ter um client com base na .env

---
+++ server/MeteoroCefet.Tests/Utils.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Driver;
+
+namespace MeteoroCefet.Tests
+{
+    internal class Utils
+    {
+        public static MongoClient MongoClient() 
+        {
+            var password = File.ReadAllText("..//..//..//..//.env").Split().Last()["MONGO_PASSWORD=".Length..];
+            var connectionString = "mongodb+srv://cefetmeteoro:SENHA@meteorocefetcluster.kvvv7gn.mongodb.net/?retryWrites=true&w=majority".Replace("SENHA", password);
+            return new MongoClient(connectionString);
+        }
+    }
+}
