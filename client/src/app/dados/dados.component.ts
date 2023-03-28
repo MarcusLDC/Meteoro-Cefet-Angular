731a95ebdc8e91ea30af33c88b38b663add2f419
Marcus Lima <marcusv2001@gmail.com>
2023-03-28 16:47:58 -0300

    Making the map interactive

--- client/src/app/dados/dados.component.ts
+++ client/src/app/dados/dados.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DadosTempo } from '../shared/models/dados-tempo-model';
 import { Estacao } from '../shared/models/estacao-model';
 import { MeteoroServices } from '../shared/services/meteoro-services';
@@ -12,26 +12,26 @@ import * as L from 'leaflet';
   styleUrls: ['./dados.component.scss']
 })
 
-export class DadosComponent implements OnInit{
+export class DadosComponent implements OnInit {
 
   displayedColumns: string[] = [ //ordem das colunas
-    'Data e Hora', 
-    'Estação', 
+    'Data e Hora',
+    'Estação',
     'Temperatura Ar',
-    'Umidade Relativa Ar', 
-    'Índice Calor', 
-    'Temperatura Ponto de Orvalho', 
-    'Pressão', 
-    'Precipitação', 
+    'Umidade Relativa Ar',
+    'Índice Calor',
+    'Temperatura Ponto de Orvalho',
+    'Pressão',
+    'Precipitação',
     'Direção Vento',
-    'Velocidade Vento', 
-    'Deficit Pressão Vapor', 
-    'Radiação Solar', 
-    'Extra1', 
-    'Extra2', 
-    'Extra3', 
-    'Extra4', 
-    'Bateria', 
+    'Velocidade Vento',
+    'Deficit Pressão Vapor',
+    'Radiação Solar',
+    'Extra1',
+    'Extra2',
+    'Extra3',
+    'Extra4',
+    'Bateria',
     'Status',
   ];
 
@@ -47,34 +47,34 @@ export class DadosComponent implements OnInit{
 
   myIcon = L.icon({
     iconUrl: '/favicon.ico',
-    iconSize: [38, 38],
-    iconAnchor: [19, 38],
+    iconSize: [30, 30],
+    iconAnchor: [19, 30],
     popupAnchor: [0, -38]
   });
 
-  constructor(private meteoroServices: MeteoroServices, private localStorage: LocalStorageServices, private builder: FormBuilder){
+  constructor(private meteoroServices: MeteoroServices, private localStorage: LocalStorageServices, private builder: FormBuilder) {
     this.form = builder.group({
       estacao: ['Tudo', Validators.required]
     });
   }
 
   async ngOnInit(): Promise<void> {
-  
+
     this.meteoroServices.getEstacoes().subscribe(x => {
       this.estacoes = x;
       this.setSelectedEstacao((this.form.get('estacao')?.value));
     });
 
-    let estacaoStorage = await this.localStorage.get<string>('estacao')??'Tudo';
+    let estacaoStorage = await this.localStorage.get<string>('estacao') ?? 'Tudo';
 
     this.form.get('estacao')?.setValue(estacaoStorage);
-    
+
     this.atualizarDados();
     setInterval(() => {
       this.atualizarDados();
     }, 60000);
 
-    setInterval(() =>{
+    setInterval(() => {
       this.gmtDateTime = new Date().toUTCString();
     }, 1000);
   }
@@ -86,58 +86,62 @@ export class DadosComponent implements OnInit{
   }
 
   private atualizarDados() {
-    if(this.form.get('estacao')?.value == 'Tudo'){
-      this.meteoroServices.getDados(1).subscribe(x =>{
+    if (this.form.get('estacao')?.value == 'Tudo') {
+      this.meteoroServices.getDados(1).subscribe(x => {
         this.dataSource = x
       });
     }
-    else{
+    else {
       this.meteoroServices.getDadosEstacao(Number(this.form.get('estacao')?.value), 1).subscribe(x => {
         this.dataSource = x,
-        this.firstDataHora = this.dataSource[0].dataHora;
+          this.firstDataHora = this.dataSource[0].dataHora;
       });
     }
   }
 
-  private setSelectedEstacao(num: string){
+  private setSelectedEstacao(num: string) {
     this.estacaoSelecionada = this.estacoes.find(x => x.numero === Number(num));
 
-    if(this.map != undefined && this.criado){
+    if (this.map != undefined && this.criado) {
       this.map.remove();
       this.criado = false;
     }
 
-    if(this.estacaoSelecionada != undefined){
+    if (this.estacaoSelecionada != undefined) {
       this.criado = true;
-
-      this.map = L.map('map', {scrollWheelZoom: false}).setView([this.estacaoSelecionada.latitude, this.estacaoSelecionada.longitude], 16);
-      
+      this.map = L.map('map', { scrollWheelZoom: false }).setView([this.estacaoSelecionada.latitude, this.estacaoSelecionada.longitude], 16);
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: 
+        attribution:
           'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
           'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18
+        maxZoom: 19
       }).addTo(this.map);
-      this.estacoes.forEach(estacao => {
-        L.marker([estacao.latitude, estacao.longitude], {icon: this.myIcon}).addTo(this.map);
-      });
+      this.markAll();
 
-    }else{
+    } else {
+
       this.criado = true;
-
-      this.map = L.map('map', {scrollWheelZoom: false,}).setView([-22.4,-43.5], 8);
-
+      this.map = L.map('map', { scrollWheelZoom: false, }).setView([-22, -43.5], 7);
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: 
+        attribution:
           'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
           'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18
+        maxZoom: 19
       }).addTo(this.map);
-      this.estacoes.forEach(estacao => {
-        L.marker([estacao.latitude, estacao.longitude], {icon: this.myIcon}).addTo(this.map);
-      });
+      this.markAll();
     }
+  }
+
+  private markAll() {
+    this.estacoes.forEach(estacao => {
+      L.marker([estacao.latitude, estacao.longitude], { icon: this.myIcon }).addTo(this.map).on('click', () =>{
+        this.setSelectedEstacao(String(estacao.numero));
+        this.form.setValue({estacao: String(estacao.numero)});
+        this.localStorage.set('estacao', String(estacao.numero));
+        this.atualizarDados();
+      });
+    });
   }
 }
