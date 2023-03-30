136c1ba7b34090f06f17cdccd7ba1a3d1cb47cd7
Marcus Lima <marcusv2001@gmail.com>
2023-03-30 16:13:19 -0300

    Implemented new UsersRepository, as well as a newUserEndpoint and password hash

---
+++ server/MeteoroCefet.Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿
+namespace MeteoroCefet.Domain.Entities
+{
+    public class ApplicationUser : Entity
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
---
+++ server/MeteoroCefet.Infra/UsersRepository.cs
@@ -0,0 +1,22 @@
+﻿using MeteoroCefet.Domain.Entities;
+using MongoDB.Driver;
+using System.ComponentModel;
+
+namespace MeteoroCefet.Infra
+{
+    public class UsersRepository : MongoRepository<ApplicationUser>
+    {
+        public UsersRepository(IMongoClient mongoClient) : base(mongoClient)
+        {
+        }
+        public async Task<List<ApplicationUser>> GetAll()
+        {
+            return await Collection.Find(_ => true).ToListAsync();
+        }
+        public async Task<ApplicationUser> GetByUsername(string username)
+        {
+            return await Collection.Find(ApplicationUser => ApplicationUser.Username == username)
+                .FirstOrDefaultAsync();
+        }
+    }
+}
