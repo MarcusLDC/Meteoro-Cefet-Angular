3ba37783bf659d60aad6f21f5a4612d474d9cfb0
Marcus Lima <marcusv2001@gmail.com>
2023-03-31 15:11:03 -0300

    New auth services and admin config component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -14,6 +14,7 @@ export class AppComponent {
   buttonMenu!: HTMLElement | null;
   divDropdown!: HTMLElement | null;
   logado: boolean = false;
+  admin: boolean = false;
 
   constructor(private auth: AuthService){}
 
@@ -30,6 +31,11 @@ export class AppComponent {
     this.logado = await this.auth.isLogged();
     setInterval(async () => {
       this.logado = await this.auth.isLogged();
+    }, 30000);
+
+    this.admin = await this.auth.isAdmin();
+    setInterval(async () => {
+      this.admin = await this.auth.isAdmin();
     }, 30000);
   }
 } 
