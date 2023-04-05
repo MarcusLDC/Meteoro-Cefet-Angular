1491f09af4dd15635911014736c4b43acee175e4
João Xavier <fastjonh@gmail.com>
2023-04-05 02:22:20 -0300

    removed

--- server/MeteoroCefet.Domain/Entities/ApplicationUser.cs
+++ server/MeteoroCefet.Domain/Entities/ApplicationUser.cs
@@ -5,7 +5,7 @@ using MongoDbGenericRepository.Attributes;
 namespace MeteoroCefet.Domain.Entities.Identity
 {
     [CollectionName("Users")]
-    public class ApplicationUser : MongoIdentityUser<Guid>, IEntity
+    public class ApplicationUser : MongoIdentityUser<Guid>
     {
         public string Username { get; set; }
         public string Password { get; set; }
