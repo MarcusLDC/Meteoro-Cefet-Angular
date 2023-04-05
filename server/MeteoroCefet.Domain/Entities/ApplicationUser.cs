794a88763d14f74be1608e8b98ad90ca9c9636dc
João Xavier <fastjonh@gmail.com>
2023-04-05 03:06:45 -0300

    not needed

--- server/MeteoroCefet.Domain/Entities/ApplicationUser.cs
+++ server/MeteoroCefet.Domain/Entities/ApplicationUser.cs
@@ -7,7 +7,5 @@ namespace MeteoroCefet.Domain.Entities.Identity
     [CollectionName("Users")]
     public class ApplicationUser : MongoIdentityUser<Guid>
     {
-        public string Username { get; set; }
-        public string Password { get; set; }
     }
 }
