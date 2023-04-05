0b7f5db9caa1d6f3a92a0213622ca061cb484007
João Xavier <fastjonh@gmail.com>
2023-04-05 02:15:51 -0300

    updating to use IdentityService

--- server/MeteoroCefet.API/Endpoints/LoginEndpoint.cs
+++ server/MeteoroCefet.API/Endpoints/LoginEndpoint.cs
@@ -1,11 +1,5 @@
-﻿
-using Microsoft.AspNetCore.Mvc;
-using System.IdentityModel.Tokens.Jwt;
-using Microsoft.IdentityModel.Tokens;
-using MeteoroCefet.Domain.Entities;
-using Microsoft.AspNetCore.Identity;
-using System.Text;
-using System.Security.Claims;
+﻿using Microsoft.AspNetCore.Mvc;
+using MeteoroCefet.Application;
 
 namespace MeteoroCefet.API.Endpoints
 {
@@ -15,56 +9,19 @@ namespace MeteoroCefet.API.Endpoints
         {
             app.MapPost("/login", Handler);
         }
-        private static async Task<AuthorizationDTO> Handler([FromServices] ILogger<ApplicationUser> log, [FromServices] UserManager<ApplicationUser> userManager, [FromBody] UserInformationDTO userDTO)
+
+        private static async Task<UserLoginResponse> Handler([FromServices] ILogger<LoginEndpoint> log, [FromServices] IdentityService identityService, [FromBody] UserLoginRequest loginRequest)
         {
-            var usuario = await userManager.FindByNameAsync(userDTO.Username);
+            var response = await identityService.Login(loginRequest);
 
-            if (usuario is null)
+            if (!response.Success)
             {
-                log.LogInformation("Tentativa de login com usuário inválido: {userDTO.Username}", userDTO.Username);
-
-                return new() { Success = false, Message = "Credenciais incorretas"};
+                log.LogInformation("Tentativa de login com usuário inválido: {userDTO.Username}", loginRequest.Username);
             }
 
-            bool acesso = await userManager.CheckPasswordAsync(usuario, userDTO.Password);
+            log.LogInformation("Tentativa de login: {userDTO.Username} , Status de acesso: {acesso} ", loginRequest.Username, response.Success);
 
-            log.LogInformation("Tentativa de login: {userDTO.Username} , Status de acesso: {acesso} ", userDTO.Username, acesso);
-
-            if (acesso)
-            {
-                return new() { Success = true, Jwt = await GenerateTokenAsync(userManager, usuario)};
-            }
-
-            return new() { Success = false, Message = "Credenciais incorretas" };
-        }
-        private static async Task<string> GenerateTokenAsync(Microsoft.AspNetCore.Identity.UserManager<ApplicationUser> userManager, ApplicationUser usuario)
-        {
-            var claims = new List<Claim>
-            {
-                new Claim(JwtRegisteredClaimNames.Sub, usuario.Username),
-                new Claim(JwtRegisteredClaimNames.Jti, usuario.Id.ToString()),
-            };
-
-            var userRoles = await userManager.GetRolesAsync(usuario);
-
-            foreach (var role in userRoles)
-            {
-                claims.Add(new Claim(ClaimTypes.Role, role));
-            }
-
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("senhasecretadocefet"));
-            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-            var expires = DateTime.Now.AddDays(1);
-
-            var token = new JwtSecurityToken(
-                issuer: "Comet-Lapa",
-                audience: "MeteoroCefet",
-                claims,
-                expires: expires,
-                signingCredentials: creds
-            );
-
-            return new JwtSecurityTokenHandler().WriteToken(token);
+            return response;
         }
     }
 }
