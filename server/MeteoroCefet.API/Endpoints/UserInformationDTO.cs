1c11c15f69c0e22089061e0c77717167c2d910c8
João Xavier <fastjonh@gmail.com>
2023-04-05 02:16:30 -0300

    not needed

--- server/MeteoroCefet.API/Endpoints/UserInformationDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MeteoroCefet.API.Endpoints
-{
-    public class UserInformationDTO
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
