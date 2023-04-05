f424200576bec2ba967ad30b0d3b494f2a3ba200
João Xavier <fastjonh@gmail.com>
2023-04-05 02:18:09 -0300

    configuring authentication and fixing mongoDbStores change of guid representation mode

--- server/MeteoroCefet.API/Program.cs
+++ server/MeteoroCefet.API/Program.cs
@@ -1,48 +1,24 @@
 using MeteoroCefet.API;
-using MeteoroCefet.Domain.Entities;
 using MeteoroCefet.Domain.Entities.Identity;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
+using MongoDB.Bson;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder
+    .Services
+    .AddIdentity<ApplicationUser, ApplicationRole>()
+    .AddMongoDbStores<ApplicationUser, ApplicationRole, Guid>(builder.Configuration.GetMongoConnectionString(), "Identity")
+    .AddDefaultTokenProviders();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddCors();
 builder.Services.ConfigureMeteoroServices();
-builder.Services.AddSingleton<BCrypt.Net.BCrypt>();
+builder.Services.ConfigureAuthorization(builder.Configuration);
+builder.Services.ConfigureMongoClient(builder.Configuration);
 
-builder.Services
-.AddIdentity<ApplicationUser, ApplicationRole>()
 
-.AddMongoDbStores<ApplicationUser, ApplicationRole, Guid>
-(
-    builder.GetConnectionString(), "Identity"
-);
-
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = "Comet-Lapa",
-            ValidAudience = "MeteoroCefet",
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("senhasecretadocefet"))
-        };
-    });
 
 builder.Logging.AddConsole();
-builder.ConfigureMongoClient();
 
 var app = builder.Build();
 
@@ -55,4 +31,9 @@ if (app.Environment.IsDevelopment())
 app.UseCors(b => b.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 app.UseCustomExceptionHandler();
 app.UseEndpointDefinitions();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+BsonDefaults.GuidRepresentation = GuidRepresentation.CSharpLegacy;
 app.Run();
