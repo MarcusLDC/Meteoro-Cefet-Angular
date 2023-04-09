82fc354ac08dbf0eb2de03d9c90652228448f28e
João Xavier <fastjonh@gmail.com>
2023-04-09 07:56:11 -0300

    making sure earliest date gets biggest priority

---
+++ server/MeteoroCefet.Tests/TaskManagerTests.cs
@@ -0,0 +1,24 @@
+﻿using MeteoroCefet.API.BackgroundServices;
+
+namespace MeteoroCefet.Tests
+{
+    internal class TaskManagerTests
+    {
+        [Test]
+        public async Task GetAggregated()
+        {
+            var tm = new TaskManager();
+
+            var now = DateTime.Now;
+
+            tm.Enqueue(new() { Activity = async (ct) => { }, ExecutionTime = now });
+            tm.Enqueue(new() { Activity = async (ct) => { }, ExecutionTime = now.AddDays(1) });
+
+            var first = tm.Dequeue();
+            Assert.IsTrue(first.ExecutionTime == now);
+
+            var second = tm.Dequeue();
+            Assert.IsTrue(second.ExecutionTime > now);
+        }
+    }
+}
