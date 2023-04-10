a2d8c0fa9cb245f8841603609f7d700a4774450c
Marcus Lima <marcusv2001@gmail.com>
2023-04-09 23:25:23 -0300

    Implemented CSV generator with proper filters

--- server/MeteoroCefet.API/Endpoints/ConsultaEndpoint.cs
+++ server/MeteoroCefet.API/Endpoints/ConsultaEndpoint.cs
@@ -1,8 +1,16 @@
-﻿using MeteoroCefet.Application.Models;
+﻿using Amazon.Runtime.Internal.Transform;
+using MeteoroCefet.Application.Models;
 using MeteoroCefet.Infra;
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Driver;
 using MongoDB.Driver.Linq;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using CsvHelper;
+using CsvHelper.Configuration;
+using DnsClient.Protocol;
+using System.Text;
 
 namespace MeteoroCefet.API.Endpoints
 {
@@ -13,57 +21,99 @@ namespace MeteoroCefet.API.Endpoints
             app.MapPost("/consulta", Handler);
         }
 
-        private static async Task<List<ConsultaResultModel>> Handler([FromServices] DadosTempoRepository repository, [FromBody] ConsultaModel model)
+        private static async Task<File> Handler([FromServices] DadosTempoRepository repository, [FromBody] ConsultaModel model)
+        {
+            IEnumerable<Dictionary<string, object?>> stationsAverageData = await GetConsultaData(repository, model);
+
+            var memoryStream = new MemoryStream();
+            var writer = new StreamWriter(memoryStream);
+            var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
+
+            WriteHeader(stationsAverageData, csv);
+            WriteRecords(stationsAverageData, csv);
+            csv.Flush();
+
+
+            return new File(Convert.ToBase64String(memoryStream.ToArray()), "text/csv" , "dados.csv");
+        }
+
+        public record File(string Data, string Type, string Name);
+
+        private static async Task<IEnumerable<Dictionary<string, object?>>> GetConsultaData(DadosTempoRepository repository, ConsultaModel model)
         {
             model.PeriodoFim = model.PeriodoFim.AddHours(23).AddMinutes(59);
 
-            var result = await repository
-                .Collection
+            var result = await repository.Collection
                 .Find(x => x.DataHora >= model.PeriodoInicio && x.DataHora <= model.PeriodoFim && model.Estacao.Contains(x.Estacao))
                 .SortBy(x => x.Estacao)
                 .ToListAsync();
 
-            var groupedIntervalo = result.GroupBy(x => new {
+            var groupedByIntervalo = result.GroupBy(x => new
+            {
                 x.Estacao,
                 DataHora = GetIntervaloDataHora(x.DataHora, model.Intervalo)
-            })
-                        .Select(g => new ConsultaResultModel
-                        {
-                            DataHora = g.Key.DataHora,
-                            Estacao = g.Key.Estacao,
-                            TempAr = Math.Round(g.Average(x => x.TemperaturaAr), 2),
-                            TempMin = Math.Round(g.Min(x => x.TemperaturaAr), 2),      
-                            TempMax = Math.Round(g.Max(x => x.TemperaturaAr), 2) ,       // > Filtro para ignorar campos
-                            TempOrv = Math.Round(g.Average(x => x.TempPontoOrvalho), 2),    
-                            Chuva = Math.Round(g.Average(x => x.Precipitacao), 2),         
-                            DirecaoVento = Math.Round(g.Average(x => x.DirecaoVento), 2),      // > Produzir o CSV e o grafico no backend
-                            VelocidadeVento = Math.Round(g.Average(x => x.VelocidadeVento), 2),
-                            VelocidadeVentoMax = g.Max(x => x.VelocidadeVento),
-                            Bateria = Math.Round(g.Average(x => x.Bateria), 2),
-                            Radiacao = Math.Round(g.Average(x => x.RadSolar), 2),
-                            PressaoATM = Math.Round(g.Average(x => x.Pressao), 2),
-                            IndiceCalor = Math.Round(g.Average(x => x.IndiceCalor), 2),
-                            UmidadeRelativa = Math.Round(g.Average(x => x.Extra2), 2),
-                        })
-                        .ToList();
+            });
 
-            return groupedIntervalo;
+            var stationsAverageData = groupedByIntervalo.Select(g => new Dictionary<string, object?>
+                {
+                    { "Data Hora (UTC-3)" , g.Key.DataHora.AddHours(-3).ToString("g", new CultureInfo("pt-BR"))},
+                    { "Estação" , g.Key.Estacao },
+
+                    { "Temp. Ar" , model.TempAr ? Math.Round(g.Average(x => x.TemperaturaAr), 2) : null},
+                    { "Temp. Min" , model.TempMin ? Math.Round(g.Min(x => x.TemperaturaAr), 2) : null},
+                    { "Temp. Max" , model.TempMax ? Math.Round(g.Max(x => x.TemperaturaAr), 2) : null},
+                    { "Temp. Orv" , model.TempOrv ? Math.Round(g.Average(x => x.TempPontoOrvalho), 2) : null},
+
+                    { "Chuva" , model.Chuva ? Math.Round(g.Average(x => x.Precipitacao), 2) : null},
+                    { "Direção Vento" , model.DirecaoVento ? Math.Round(g.Average(x => x.DirecaoVento), 2) : null},
+                    { "VelocidadeVento" , model.VelocidadeVento ? Math.Round(g.Average(x => x.VelocidadeVento), 2) : null},
+                    { "VelocidadeVentoMax" , model.VelocidadeVentoMax ? g.Max(x => x.VelocidadeVento) : null},
+
+                    { "Bateria" , model.Bateria ? Math.Round(g.Average(x => x.Bateria), 2) : null},
+                    { "Radiação" , model.Radiacao ? Math.Round(g.Average(x => x.RadSolar), 2) : null},
+                    { "Pressão ATM" , model.PressaoATM ? Math.Round(g.Average(x => x.Pressao), 2) : null},
+                    { "Índice Calor" , model.IndiceCalor ? Math.Round(g.Average(x => x.IndiceCalor), 2) : null},
+                    { "Umidade Relativa" , model.UmidadeRelativa ?  Math.Round(g.Average(x => x.Extra2), 2) : null}
+                })
+                .Select(x => x.Where(y => y.Value != null)
+                .ToDictionary(x => x.Key, x => x.Value));
+            return stationsAverageData;
         }
+
+        private static void WriteRecords(IEnumerable<Dictionary<string, object?>> stationsAverageData, CsvWriter csv)
+        {
+            foreach (var stationAverageData in stationsAverageData)
+            {
+                foreach (var averageData in stationAverageData)
+                {
+                    csv.WriteField(averageData.Value);
+                }
+                csv.NextRecord();
+            }
+        }
+
+        private static void WriteHeader(IEnumerable<Dictionary<string, object?>> stationsAverageData, CsvWriter csv)
+        {
+            foreach (var key in stationsAverageData.First().Keys)
+            {
+                csv.WriteField(key);
+            }
+
+            csv.NextRecord();
+        }
+
         private static DateTime GetIntervaloDataHora(DateTime date, string intervalo)
         {
-            var dataHora = intervalo switch
+            return intervalo switch
             {
                 "1 minuto" => new DateTime(date.Year, date.Month, date.Day, date.Hour, date.Minute / 1 * 1, 0),
                 "10 minutos" => new DateTime(date.Year, date.Month, date.Day, date.Hour, date.Minute / 10 * 10, 0),
                 "30 minutos" => new DateTime(date.Year, date.Month, date.Day, date.Hour, date.Minute / 30 * 30, 0),
-                "1 hora" => new DateTime(date.Year, date.Month, date.Day, date.Hour, date.Minute / 60 * 60, 0),
+                "1 hora" => new DateTime(date.Year, date.Month, date.Day, date.Hour, 0, 0),
                 "24 horas" => new DateTime(date.Year, date.Month, date.Day, 0, 0, 0),
                 "Mensal" => new DateTime(date.Year, date.Month, 1, 0, 0, 0),
                 _ => throw new ArgumentException("Intervalo inválido"),
             };
-            return dataHora;
         }
     }
 }
-
-
