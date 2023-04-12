2cf68d762ff484a3934fc55ca57e9e27150a8b73
João Xavier <fastjonh@gmail.com>
2023-04-12 15:49:34 -0300

    criando endpoktjs

---
+++ server/MeteoroCefet.API/Endpoints/DadosCountEndpoint.cs
@@ -0,0 +1,18 @@
+﻿using MeteoroCefet.Infra;
+using MongoDB.Driver;
+
+namespace MeteoroCefet.API.Endpoints
+{
+    public class DadosCountEndpoint : IEndpointDefinition
+    {
+        public void DefineEndpoints(WebApplication app)
+        {
+            app.MapGet("/dados/total", Handler);
+        }
+
+        private static async Task<long> Handler(DadosTempoRepository repository)
+        {
+            return await repository.Collection.CountDocumentsAsync(_ => true);
+        }
+    }
+}
