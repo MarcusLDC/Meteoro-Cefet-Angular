c7105de73f3d7a2e9f450c7a9848a1abbbded9ee
João Xavier <fastjonh@gmail.com>
2023-04-12 15:31:41 -0300

    httpClient e nao fetch......

--- client/src/app/shared/services/meteoro-services.ts
+++ client/src/app/shared/services/meteoro-services.ts
@@ -11,56 +11,63 @@ import { RetornoDTO } from "./DTOs/retorno-DTO";
 import { FileModel } from "../models/file-model";
 import { DadosGrafico } from "../models/dados-grafico-model";
 
+type GeoData = { address: { city: string | null, town: string | null, state: string } };
+
 @Injectable({ providedIn: "root" })
 export class MeteoroServices {
-    constructor(private httpClient : HttpClient){}
-    
-    public consultarTabela(model: ConsultaModel){
+    constructor(private httpClient: HttpClient) { }
+
+    public geocode(estacao: Estacao) {
+        const url = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${estacao.latitude}'&lon=${estacao.longitude}`;
+        return this.httpClient.get<GeoData>(url);
+    }
+
+    public consultarTabela(model: ConsultaModel) {
         let endpoint = `${environment.apiUrl}/consulta/tabela`
         return this.httpClient.post<FileModel>(endpoint, model)
     }
 
-    public consultarGrafico(model: ConsultaModel){
+    public consultarGrafico(model: ConsultaModel) {
         let endpoint = `${environment.apiUrl}/consulta/grafico`
         return this.httpClient.post<DadosGrafico[]>(endpoint, model)
     }
-    
-    public getDados(numPagina: number){
+
+    public getDados(numPagina: number) {
         let endpoint = `${environment.apiUrl}/dados`
         return this.httpClient.post<DadosTempo[]>(endpoint, numPagina)
     }
-    public getEstacoes(){
+    public getEstacoes() {
         let endpoint = `${environment.apiUrl}/estacoes`
         return this.httpClient.get<Estacao[]>(endpoint)
     }
-    public getDadosEstacao(numeroEstacao: number, numPagina:number){
+    public getDadosEstacao(numeroEstacao: number, numPagina: number) {
         let endpoint = `${environment.apiUrl}/dadosEstacao`
-        return this.httpClient.post<DadosTempo[]>(endpoint, {numeroEstacao, numPagina})
+        return this.httpClient.post<DadosTempo[]>(endpoint, { numeroEstacao, numPagina })
     }
 
     // authentication required
 
-    public getModeradores(){
+    public getModeradores() {
         let endpoint = `${environment.apiUrl}/moderadores`
         return this.httpClient.get<string[]>(endpoint)
     }
 
-    public novoModerador(user: UserModel, roles: string[]){
+    public novoModerador(user: UserModel, roles: string[]) {
         let endpoint = `${environment.apiUrl}/usuario/moderador/new`;
-        return this.httpClient.post<NewUserDTO>(endpoint, {username: user.username, password: user.password, roles});
+        return this.httpClient.post<NewUserDTO>(endpoint, { username: user.username, password: user.password, roles });
     }
 
-    public deleteUsuario(username: string){
+    public deleteUsuario(username: string) {
         let endpoint = `${environment.apiUrl}/usuario/delete`
-        return this.httpClient.post<RetornoDTO<{errors: string[]}>>(endpoint, {username});
+        return this.httpClient.post<RetornoDTO<{ errors: string[] }>>(endpoint, { username });
     }
 
-    public editarEstacao(estacao: Estacao){
+    public editarEstacao(estacao: Estacao) {
         let endpoint = `${environment.apiUrl}/estacoesEditar`
         return this.httpClient.post<Estacao[]>(endpoint, estacao)
     }
 
-    public login(user: UserModel){
+    public login(user: UserModel) {
         let endpoint = `${environment.apiUrl}/login`
         return this.httpClient.post<AuthenticationDTO>(endpoint, user)
     }
