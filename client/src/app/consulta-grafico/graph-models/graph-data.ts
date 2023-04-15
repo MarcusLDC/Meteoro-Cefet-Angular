b87eed151ef255ba3aae48d9a0856873a7cddba5
João Xavier <fastjonh@gmail.com>
2023-04-15 05:42:17 -0300

    adicionado graph data

---
+++ client/src/app/consulta-grafico/graph-models/graph-data.ts
@@ -0,0 +1,14 @@
+export interface GraphData{
+    type: GraphType,
+    buildDataSet: (campos: string[]) => DataSet[]
+}
+
+export enum GraphType {
+    Temperatura,
+    Chuva,
+    Vento,
+    Pressao,
+    Radiacao,
+    Bateria
+}
+export type DataSet = { label: string, data: number[], borderColor: string, fill: boolean, type: string, backgroundColor: string, yAxisID: string, z: number };
